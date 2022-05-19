4225c3f37be6bbae93a2b8576f6cf4985ddc6ac8
Hyun Jun <eppioes@gmail.com>
2022-05-19 12:59:11 +0900

    --

---
+++ Testing/TestingFee.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    common_path1 = "/Users/mac/Desktop/RLPortfolio"
+    common_path2 = "/Metrics/Profitloss_test"
+
+    # Dirichlet model
+    profitlosses1 = pd.read_csv(common_path1 + "/DirichletPortfolio (scalability +3, cost)" + common_path2).iloc[:,1]
+    # DQN model
+    profitlosses2 = pd.read_csv(common_path1 + "/DQNPortfolio (scalability +3, cost)" + common_path2).iloc[:,1]
+    # B&H
+    profitlosses3 = pd.read_csv(common_path1 + "/DirichletPortfolio (scalability +3, cost)" + "/Metrics/Profitloss B&H").iloc[:,1]
+
+
+    #Visualizing
+    fig, ax = plt.subplots(figsize=(60, 8), facecolor="w")
+    ax.get_xaxis().get_major_formatter().set_scientific(False)
+    ax.get_yaxis().get_major_formatter().set_scientific(False)
+    ax.yaxis.tick_right()
+    ax.set_facecolor("lightgray")
+    ax.set_ylabel("Profitloss")
+    ax.set_xlabel("Time step")
+    plt.title("Profitloss")
+    plt.plot(profitlosses1, label="Dirichlet")
+    plt.plot(profitlosses2, label="DQN")
+    plt.plot(profitlosses3, label="B&H")
+
+
+    xticks = [int(i) for i in np.linspace(0, len(profitlosses1), 6)]
+    plt.xticks(xticks)
+    plt.grid(True, color="w", alpha=0.5)
+    plt.legend()
+    plt.show()
+    fig.savefig("/Users/mac/Desktop/RLPortfolio/TestingScalability")
